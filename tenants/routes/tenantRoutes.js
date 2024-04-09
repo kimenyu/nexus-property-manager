9b91a04ce788ed2f44fe231b94776ee27ea0641e
kimenyu <njorogekimenyu@gmail.com>
2024-04-09 12:39:29 +0300

    adding a callback

--- tenants/routes/tenantRoutes.js
+++ tenants/routes/tenantRoutes.js
@@ -1,8 +1,8 @@
 import { Router } from 'express';
-import { mpesaCallback, makeRentDeposits } from '../controllers/makeRentDeposits.js';
+import { mpesaCallback, makeRentDeposits, generateToken } from '../controllers/makeRentDeposits.js';
 import { authenticateTenant } from '../../middleware/tenantMiddleware.js';
 
 export const tenantRouter = Router();
 
-tenantRouter.post('/api/tenant/stk/deposit', authenticateTenant, makeRentDeposits);
+tenantRouter.post('/api/tenant/stk/deposit', authenticateTenant, generateToken,makeRentDeposits);
 tenantRouter.post('/api/tenant/mpesa/callback', mpesaCallback);
