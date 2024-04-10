84a586be27851f32b32c8bfe55beaee1944dc5d0
kimenyu <njorogekimenyu@gmail.com>
2024-04-10 16:42:58 +0300

    changing index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import axios from 'axios';
 import mongoose from "mongoose";
+import Transaction from "./models/transactionModel.js";
+import Tenant from "./models/tenantModel.js";
 import { accountRouter } from "./accounts/routes/accountRoutes.js";
 import { propertyRouter } from './properties/routes/propertyRoutes.js';
 import { apartmentRouter } from './apartments/routes/apartmentRoutes.js';
-import { mpesaRouter } from './tenants/controllers/makeRentDeposits.js';
 
 
 
@@ -44,6 +46,107 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
     });
 
 
+const generateToken = async (req, res, next) => {
+  const auth = new Buffer.from(`${process.env.SAFARICOM_CONSUMER_KEY}:${process.env.SAFARICOM_CONSUMER_SECRET}`).toString('base64');
 
+  await axios.get("https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials", {
+      headers: {
+          authorization: `Basic ${auth}`
+      },
+  }
+  ).then((response) => {
+      // console.log(data.data.access_token);
+      token = response.data.access_token;
+      console.log(token);
+      next();
+  }).catch((err) => {
+      console.log(err);
+  })
+}
 
-app.use(accountRouter, propertyRouter, apartmentRouter, mpesaRouter);
+app.post("/stk", generateToken, async(req, res) => {
+  const { tenantId, amount } = req.body;
+  const tenant = await Tenant.findById(tenantId);
+  const date = new Date();
+
+  const timestamp = 
+  date.getFullYear().toString() +
+  ("0" + (date.getMonth() + 1)).slice(-2) +
+  ("0" + date.getDate()).slice(-2) +
+  ("0" + date.getHours()).slice(-2) +
+  ("0" + date.getMinutes()).slice(-2) +
+  ("0" + date.getSeconds()).slice(-2);
+
+  const password = new Buffer.from(process.env.BUSINESS_SHORT_CODE + process.env.PASS_KEY + timestamp).toString('base64');
+
+  await axios.post (
+      "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+      {
+          BusinessShortCode: process.env.BUSINESS_SHORT_CODE,
+          Password: password,
+          Timestamp: timestamp,
+          TransactionType: "CustomerPayBillOnline",
+          Amount: amount,
+          PartyA: tenant.phone.slice(1), // Use the tenant's phone number here
+          PartyB: process.env.BUSINESS_SHORT_CODE,
+          PhoneNumber: tenant.phone.slice(1),
+          CallBackURL: 'https://nexus-property-manager.onrender.com/callback',
+          AccountReference: "Moja Nexus",
+          TransactionDesc: "Paid online",
+
+      },
+      {
+          headers: {
+              Authorization: `Bearer ${token}`,
+          },
+      }
+  ).then ((data) => {
+      res.status(200).json(data.data);
+  }).catch((err) => {
+      console.log(err.message);
+  })
+})
+
+app.post('/callback', async(req, res) => {
+  const callbackData = req.body;
+
+  // Log the callback data to the console
+  console.log(callbackData.Body);
+
+  if(!callbackData.Body.stkCallback.CallbackMetadata) {
+      console.log(callbackData.Body);
+      return res.json('ok');
+  }
+
+  console.log(callbackData.Body.stkCallback.CallbackMetadata);
+
+  const newTransaction = new Transaction({
+      MpesaReceiptNumber: callbackData.Body.stkCallback.CallbackMetadata.Item[1].Value,
+      amount: callbackData.Body.stkCallback.CallbackMetadata.Item[0].Value,
+      TransactionDate: callbackData.Body.stkCallback.CallbackMetadata.Item[3].Value,
+      MerchantRequestID: callbackData.Body.stkCallback.MerchantRequestID,
+      CheckoutRequestID: callbackData.Body.stkCallback.CheckoutRequestID,
+      ResultCode: callbackData.Body.stkCallback.ResultCode,
+      ResultDesc: callbackData.Body.stkCallback.ResultDesc,
+      PhoneNumber: callbackData.Body.stkCallback.CallbackMetadata.Item[4].Value,
+      type: 'deposit',
+      status: 'completed',
+  });
+
+  if (callbackData.Body.stkCallback.CallbackMetadata.Item[0].Value < 0 || callbackData.Body.stkCallback.ResultCode !== 0 || callbackData.Body.stkCallback.ResultDesc !== 'The service request is processed successfully.') {
+      newTransaction.status = 'failed';
+  }
+
+  await newTransaction.save();
+
+  const tenant = await Tenant.findOne({ phone: callbackData.Body.stkCallback.CallbackMetadata.Item[4].Value });
+
+  if (tenant) {
+      tenant.transactions.push(newTransaction._id);
+      await tenant.save();
+  }
+
+  return res.json('ok');
+})
+
+app.use(accountRouter, propertyRouter, apartmentRouter);
