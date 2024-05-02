cf8757bb3dec4c89e387021c62ff6da888295075
jakatsa <jakatsa5@gmail.com>
2024-05-02 14:04:14 +0300

    adding pay rent feature

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ const generateToken = async (req, res, next) => {
   })
 }
 
+//leasing and paying deposit
 app.post("/stk", generateToken, async (req, res) => {
     const { tenantId, amount } = req.body;
   
@@ -202,6 +203,146 @@ app.post('/callback', async (req, res) => {
         console.error('Error saving transaction or updating tenant:', error);
         return res.status(500).json({ message: 'Error saving transaction or updating tenant' });
     }
+});
+
+
+//paying rents
+app.post("/stk/payrent", generateToken, async (req, res) => {
+  const { tenantId, amount } = req.body;
+
+  try {
+    const tenant = await Tenant.findById(tenantId);
+    if (!tenant) {
+      return res.status(404).json({ message: 'Tenant not found' });
+    }
+
+    const date = new Date();
+    const timestamp =
+      date.getFullYear().toString() +
+      ("0" + (date.getMonth() + 1)).slice(-2) +
+      ("0" + date.getDate()).slice(-2) +
+      ("0" + date.getHours()).slice(-2) +
+      ("0" + date.getMinutes()).slice(-2) +
+      ("0" + date.getSeconds()).slice(-2);
+
+    const password = new Buffer.from(process.env.BUSINESS_SHORT_CODE + process.env.PASS_KEY + timestamp).toString('base64');
+
+    await axios.post(
+      "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+      {
+        BusinessShortCode: process.env.BUSINESS_SHORT_CODE,
+        Password: password,
+        Timestamp: timestamp,
+        TransactionType: "CustomerPayBillOnline",
+        Amount: amount,
+        PartyA: tenant?.phone?.slice(1), // Use optional chaining to safely access properties
+        PartyB: process.env.BUSINESS_SHORT_CODE,
+        PhoneNumber: tenant?.phone?.slice(1),
+        CallBackURL: 'https://nexus-property-manager.onrender.com/callback/rent',
+        AccountReference: "Moja Nexus",
+        TransactionDesc: "Paid online",
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    ).then((data) => {
+      res.status(200).json(data.data);
+    }).catch((err) => {
+      console.log(err.message);
+      res.status(500).json({ message: 'Internal server error' });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});  
+
+app.post('/callback/rent', async (req, res) => {
+  const callbackData = req.body;
+
+  // Log the callback data to the console
+  console.log(callbackData.Body);
+
+  if (!callbackData.Body.stkCallback.CallbackMetadata) {
+      console.log(callbackData.Body);
+      return res.json('ok');
+  }
+
+  console.log(callbackData.Body.stkCallback.CallbackMetadata);
+
+  // Extract TransactionDate from CallbackMetadata Item array
+  const transactionDateItem = callbackData.Body.stkCallback.CallbackMetadata.Item.find(item => item.Name === 'TransactionDate');
+
+  let transactionDate;
+  if (transactionDateItem) {
+      // Parse the TransactionDate value to a Date object
+      transactionDate = new Date(transactionDateItem.Value);
+  } else {
+      console.error('TransactionDate not found in callback data.');
+      return res.status(400).json({ message: 'TransactionDate not found in callback data' });
+  }
+
+  // Ensure transactionDate is a valid Date object
+  if (isNaN(transactionDate.getTime())) {
+      console.error('Invalid TransactionDate value:', transactionDateItem.Value);
+      return res.status(400).json({ message: 'Invalid TransactionDate value' });
+  }
+
+  const newTransaction = new Transaction({
+      MpesaReceiptNumber: callbackData.Body.stkCallback.CallbackMetadata.Item.find(item => item.Name === 'MpesaReceiptNumber')?.Value,
+      Amount: parseFloat(callbackData.Body.stkCallback.CallbackMetadata.Item.find(item => item.Name === 'Amount')?.Value),
+      TransactionDate: transactionDate, // Use parsed TransactionDate value
+      MerchantRequestID: callbackData.Body.stkCallback.MerchantRequestID,
+      CheckoutRequestID: callbackData.Body.stkCallback.CheckoutRequestID,
+      ResultCode: callbackData.Body.stkCallback.ResultCode,
+      ResultDesc: callbackData.Body.stkCallback.ResultDesc,
+      phone: callbackData.Body.stkCallback.CallbackMetadata.Item.find(item => item.Name === 'PhoneNumber')?.Value,
+      type: 'rent',
+      status: 'completed',
+  });
+
+
+  if (
+      callbackData.Body.stkCallback.ResultCode !== 0
+  ) {
+      newTransaction.status = 'failed';
+  }
+
+  try {
+      // Save the new transaction
+      await newTransaction.save();
+
+      // Construct the phone number with the country code
+      const phoneNumberWithCode = `+${callbackData.Body.stkCallback.CallbackMetadata.Item.find(item => item.Name === 'PhoneNumber')?.Value}`;
+      console.log('Phone Number with Code:', phoneNumberWithCode);
+
+      // Find the tenant by phone number (including country code)
+      const tenant = await Tenant.findOne({ phone: phoneNumberWithCode });
+
+      // Log the found tenant
+      console.log('Found tenant:', tenant);
+
+      if (tenant) {
+          // Push the new transaction ID into the tenant's transactions array
+          tenant.transactions.push(newTransaction._id);
+
+          // Save the updated tenant
+          await tenant.save();
+
+          // Log the updated tenant
+          console.log('Updated tenant:', tenant);
+
+          return res.json('ok');
+      } else {
+          console.log('Tenant not found');
+          return res.status(404).json({ message: 'Tenant not found' });
+      }
+  } catch (error) {
+      console.error('Error saving transaction or updating tenant:', error);
+      return res.status(500).json({ message: 'Error saving transaction or updating tenant' });
+  }
 });
 
 app.use(accountRouter, propertyRouter, apartmentRouter, tenantRouter);
